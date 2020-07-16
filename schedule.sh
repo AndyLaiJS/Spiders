657f280d3406742956bc08d58956206c7b288ba0
AndyPhoenix9879 <lai9879m@gmail.com>
2020-07-16 10:01:20 +0800

    schedule.sh: Precautionary removal

--- schedule.sh
+++ schedule.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-scrapy-do-cl push-project
-scrapy-do-cl schedule-job --project jobs --spider adecco --when 'every day at 01:00'
-scrapy-do-cl schedule-job --project jobs --spider cpjobs --when 'every day at 01:30'
-#scrapy-do-cl schedule-job --project jobs --spider ctgoodjobs --when 'every day at 02:00'
-scrapy-do-cl schedule-job --project jobs --spider efc --when 'every day at 02:30'
-scrapy-do-cl schedule-job --project jobs --spider glassdoor --when 'every day at 03:00'
-scrapy-do-cl schedule-job --project jobs --spider indeed_update --when 'every day at 03:30'
-scrapy-do-cl schedule-job --project jobs --spider jobsdb --when 'every day at 04:00'
-scrapy-do-cl schedule-job --project jobs --spider jora --when 'every day at 04:30'
-scrapy-do-cl schedule-job --project jobs --spider michaelpage --when 'every day at 05:00'
-scrapy-do-cl schedule-job --project jobs --spider pagepersonnel --when 'every day at 05:30'
-scrapy-do-cl schedule-job --project jobs --spider randstad --when 'every day at 06:00'
+# scrapy-do-cl push-project
+# scrapy-do-cl schedule-job --project jobs --spider adecco --when 'every day at 01:00'
+# scrapy-do-cl schedule-job --project jobs --spider cpjobs --when 'every day at 01:30'
+# #scrapy-do-cl schedule-job --project jobs --spider ctgoodjobs --when 'every day at 02:00'
+# scrapy-do-cl schedule-job --project jobs --spider efc --when 'every day at 02:30'
+# scrapy-do-cl schedule-job --project jobs --spider glassdoor --when 'every day at 03:00'
+# scrapy-do-cl schedule-job --project jobs --spider indeed_update --when 'every day at 03:30'
+# scrapy-do-cl schedule-job --project jobs --spider jobsdb --when 'every day at 04:00'
+# scrapy-do-cl schedule-job --project jobs --spider jora --when 'every day at 04:30'
+# scrapy-do-cl schedule-job --project jobs --spider michaelpage --when 'every day at 05:00'
+# scrapy-do-cl schedule-job --project jobs --spider pagepersonnel --when 'every day at 05:30'
+# scrapy-do-cl schedule-job --project jobs --spider randstad --when 'every day at 06:00'
 #scrapy-do-cl schedule-job --project jobs --spider recruithk --when 'every day at 07:00'
